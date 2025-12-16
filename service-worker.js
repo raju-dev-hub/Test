51fe850022f807398c2f4b9b6b1717cb94d57dc2
rajumondal-tech <rajumondal.dev.@gmail.com>
2025-12-16 05:47:17 +0530

    correcte

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,22 @@
-const CACHE_NAME = "gift-v1";
+const CACHE_NAME = "gift-v2"; // IMPORTANT: version bump
 
 const FILES = [
   "./",
   "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+
+  // butterflies
   "./butterfly.png",
+  "./butterfly1.jpeg",
+  "./butterfly2.png",
+  "./butterfly3.png",
+  "./butterfly4.png",
+
+  // tulips
   "./orange.png",
   "./pink.png",
   "./pinkwhite.png",
   "./purple.png",
   "./white.png"
 ];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
